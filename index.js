099301da92372073918da9e18709b25ceef3261e
Mehdiway <armachi.mehdi@gmail.com>
2021-10-14 20:31:59 +0100

    1.0

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const puppeteer = require("puppeteer");
 var log4js = require("log4js");
 log4js.configure({
-  appenders: { cheese: { type: "file", filename: "log/debug.log" } },
-  categories: { default: { appenders: ["cheese"], level: "debug" } },
+  appenders: { LOG: { type: "file", filename: "log/debug.log" } },
+  categories: { default: { appenders: ["LOG"], level: "debug" } },
 });
-var logger = log4js.getLogger("cheese");
+var logger = log4js.getLogger("LOG");
 
 var loginURL = "http://www.albaji-academy.com/Login";
 var presenceURL = "http://www.albaji-academy.com/Espace-Etudiant/Presence/";
+
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 (async () => {
   try {
@@ -19,27 +21,23 @@ var presenceURL = "http://www.albaji-academy.com/Espace-Etudiant/Presence/";
     logger.debug("Current URL : " + page.url());
     await page.screenshot({ path: "./screenshot1.png", fullPage: true });
 
+    logger.debug("Filling login form...");
+    console.log("Filling login form...");
     await page.type("#login-form-username", "armachi.mehdi@gmail.com");
     await page.type("#login-form-password", "16199.Mehdiway");
-    console.log("filled form...");
-    logger.debug("filled form...");
     await page.screenshot({ path: "./screenshot2.png", fullPage: true });
 
-    logger.debug("1");
+    logger.debug("Click submit button...");
+    console.log("Click submit button...");
     await page.click("#submit-button");
-    logger.debug("2");
 
     await page.waitForSelector("a.si-facebook");
-    logger.debug("3");
     logger.debug("Current URL : " + page.url());
-    logger.debug("4");
     await page.screenshot({ path: "./screenshot3.png", fullPage: true });
-    logger.debug("5");
+    logger.debug("Go to : " + presenceURL);
     await page.goto(presenceURL);
-    logger.debug("6");
     await page.waitForSelector("#template-contactform-seance");
     logger.debug("Current URL : " + page.url());
-
     await page.screenshot({ path: "./screenshot4.png", fullPage: true });
 
     var nbOfOptions = await page.$eval(
@@ -55,14 +53,31 @@ var presenceURL = "http://www.albaji-academy.com/Espace-Etudiant/Presence/";
       logger.debug("Module to select : " + optionValue);
       await page.select("#template-contactform-seance", optionValue);
 
+      page.on("dialog", async (dialog) => {
+        logger.debug("Dialog popup : " + dialog.message());
+        console.log("Dialog popup : " + dialog.message());
+        logger.debug("URL : " + page.url());
+        console.log("URL : " + page.url());
+        delay(1000);
+        await dialog.dismiss();
+        delay(1000);
+      });
+
       await page.screenshot({ path: "./screenshot5.png", fullPage: true });
-      await page.click("#submit-form");
+      await page.waitForSelector("#submit-button");
+      console.log("Click submit button...");
+      await Promise.all([
+        page.waitForNavigation(),
+        page.click("#submit-button"),
+      ]);
+      await page.screenshot({ path: "./screenshot6.png", fullPage: true });
     } else {
       logger.debug("Number of options : " + nbOfOptions + ". Exiting.");
-
-      await page.screenshot({ path: "./screenshot6.png", fullPage: true });
+      await page.screenshot({ path: "./screenshot7.png", fullPage: true });
     }
 
+    logger.debug("Current URL : " + page.url());
+    console.log("Current URL : " + page.url());
     await browser.close();
   } catch (e) {
     console.log(e);
